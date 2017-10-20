8e614383537940da1cdc3ccb9177beebcf964294
DennySPB <dennyspb@gmail.com>
2017-10-20 12:07:54 +0300

    Remove duplicate packages

--- device.mk
+++ device.mk
@@ -204,12 +204,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf
 
-
-#Guyver
-PRODUCT_PACKAGES += \
-    init.spectrum.rc \
-    init.spectrum.sh
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/idc/uinput-fpc.idc:system/usr/idc/uinput-fpc.idc
