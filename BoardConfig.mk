cf732de673828cf1f33d8f10abb83ae4c1d4b34c
DennySPB <dennyspb@gmail.com>
2017-11-02 15:30:50 +0300

    Enable Realtime battery info if present

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,9 @@ TARGET_OTA_ASSERT_DEVICE := gemini
 TARGET_BOOTLOADER_BOARD_NAME := msm8996
 TARGET_NO_BOOTLOADER := true
 
+#Realtime battery info
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff androidboot.selinux=permissive
